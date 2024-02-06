5468245e6b3aa776a2ad3518ac811e54fc00afc3
Martin Eliseo Tlapa Davila <etlapa@edev.mx>
2024-02-06 10:24:01 -0600

    CBJ-1 - Getting started with eCookBook. Adding example of a List to a Map using streams

---
+++ src/main/java/collections/streams/hashMapCollector/Account.java
@@ -0,0 +1,12 @@
+package collections.streams.hashMapCollector;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Account {
+    private String accountId;
+    private String name;
+    private String accountNumber;
+}
---
+++ src/main/java/collections/streams/hashMapCollector/HashMapCollector.java
@@ -0,0 +1,41 @@
+package collections.streams.hashMapCollector;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class HashMapCollector {
+    public static void main(String[] args){
+        HashMapCollector hashMapCollector = new HashMapCollector();
+        hashMapCollector.createHashMap();
+    }
+    private void createHashMap(){
+        Map<String,String> accountNames = createList().stream().collect(Collectors.toMap(
+                Account::getAccountId,
+                account -> {
+                    StringJoiner sj = new StringJoiner(" ");
+
+                    sj.add(account.getName());
+                    sj.add("x"+account.getAccountNumber().substring(account.getAccountNumber().length()-4));
+
+                    return sj.toString();
+                }
+        ));
+
+        log.info("accountNames: {}",accountNames);
+    }
+    private List<Account> createList(){
+        List<Account> accounts = new ArrayList<>();
+
+        accounts.add(new Account("1","Account one","1234"));
+        accounts.add(new Account("2","Account two","2345"));
+        accounts.add(new Account("3","Account three","3456"));
+
+        return accounts;
+    }
+}
