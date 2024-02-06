d812d8acb60b6a2346523e875c81e4d2e7c96b43
Martin Eliseo Tlapa Davila <etlapa@edev.mx>
2024-02-06 17:49:20 -0600

    CBJ-2 - Implementing CompletableFuture example

--- src/main/java/collections/streams/listToHashMapCollector/HashMapCollector.java
+++ src/main/java/collections/streams/listToHashMapCollector/HashMapCollector.java
@@ -1,40 +1,40 @@
 package collections.streams.listToHashMapCollector;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
-@Slf4j
 public class HashMapCollector {
-    public static void main(String[] args){
-        HashMapCollector hashMapCollector = new HashMapCollector();
-        log.info("accountNames: {}",hashMapCollector.getHashMap());
-    }
-    public Map<String,String> getHashMap(){
-        Map<String,String> accountNames = createList().stream().collect(Collectors.toMap(
-                Account::getAccountId,
-                account -> {
-                    StringJoiner sj = new StringJoiner(" ");
+  public Map<String, String> getHashMap() {
+    Map<String, String> accountNames =
+        createList().stream()
+            .collect(
+                Collectors.toMap(
+                    Account::getAccountId,
+                    account -> {
+                      StringJoiner sj = new StringJoiner(" ");
 
-                    sj.add(account.getName());
-                    sj.add("x"+account.getAccountNumber().substring(account.getAccountNumber().length()-4));
+                      sj.add(account.getName());
+                      sj.add(
+                          "x"
+                              + account
+                                  .getAccountNumber()
+                                  .substring(account.getAccountNumber().length() - 4));
 
-                    return sj.toString();
-                }
-        ));
-        return accountNames;
-    }
-    private List<Account> createList(){
-        List<Account> accounts = new ArrayList<>();
+                      return sj.toString();
+                    }));
+    return accountNames;
+  }
 
-        accounts.add(new Account("1","Account one","1234"));
-        accounts.add(new Account("2","Account two","2345"));
-        accounts.add(new Account("3","Account three","3456"));
+  private List<Account> createList() {
+    List<Account> accounts = new ArrayList<>();
 
-        return accounts;
-    }
+    accounts.add(new Account("1", "Account one", "1234"));
+    accounts.add(new Account("2", "Account two", "2345"));
+    accounts.add(new Account("3", "Account three", "3456"));
+
+    return accounts;
+  }
 }
